d3141ce8942422501d5ff204b1de730b9b98bc82
Luis Felipe Cambiaghe Guedes <lfc_guedes@hotmail.com>
2024-05-03 19:27:37 -0300

    Revert "mudando ip"
    This reverts commit 479de00c682a0ebef40f89a16f49d5909b291555.

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -90,11 +90,11 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["web.newstandard.com.br"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
-    _ => "mCdVWpmQ2ppBl0rpB417sg0SVWyeKmEmhkTtorLdS6c=",
+    _ => "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=",
 };
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
